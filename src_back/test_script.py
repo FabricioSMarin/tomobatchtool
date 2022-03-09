5ba3a236e7e49d454835f67804c7f5c81f4bac23
FabrcioSMarin <fabricio.s.marin@gmail.com>
2022-03-09 15:52:52 -0600

    removing original code

--- src_back/test_script.py
+++
@@ -1,8 +0,0 @@
-
-
-import time
-x = [0,1,2]
-
-for i in range(len(x)):
-	print(i)
-	time.sleep(1)
--- src_back/test_script2.py
+++
@@ -1,8 +0,0 @@
-
-
-import time
-x = [3,4,5]
-
-for i in range(len(x)):
-	print(x[i])
-	time.sleep(1)
